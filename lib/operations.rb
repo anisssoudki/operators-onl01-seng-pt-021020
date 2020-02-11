260157a2fb22a7464208d357831660c6fc722813
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-11 01:31:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,23 @@
 def unsafe?(speed)
 
-if ((speed < 40 && speed > 60) || (speed > 95))
+if speed <= 40 
   puts TRUE
-  
-else
-  puts FALSE
-end
-end
-unsafe?(55)
+  elsif speed >= 95
+  puts false
+elsif speed >= 60
+  puts TRUE
 
+else 
+	puts true
+		
+	end
+end
+unsafe?(45)
+unsafe?(65)
+unsafe?(50)
+unsafe?(95)
+unsafe?(100)
+unsafe?(1)
 
 def not_safe?(speed)
 	
